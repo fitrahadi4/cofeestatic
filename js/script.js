364bce5b874b409671588b32bf39aa229286027f
Fitrahadi Surya Dharma <adi.psd04@gmail.com>
2023-12-02 11:43:07 +0800

    Menambah fitur keranjang dasar

--- js/script.js
+++ js/script.js
@@ -8,17 +8,25 @@ document.querySelector("#hamburger-menu").onclick = () => {
 
 // Toggle class active untuk search form
 const searchForm = document.querySelector(".search-form");
-const serachBox = document.querySelector("#search-box");
+const searchBox = document.querySelector("#search-box");
 
 document.querySelector("#search-button").onclick = (e) => {
   searchForm.classList.toggle("active"); // menambahkan class active jika belum ada, jika sudah ada, maka dihilangkan
-  serachBox.focus();
+  searchBox.focus();
   e.preventDefault(); // agar ketika di scroll, halaman tdk scroll ke atas
+};
+
+// Toggle class active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
 };
 
 // Klik di luar elemen
 const hm = document.querySelector("#hamburger-menu");
 const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -26,5 +34,8 @@ document.addEventListener("click", function (e) {
   }
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
+  }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
   }
 });
